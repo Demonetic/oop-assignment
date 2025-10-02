d9c119b40df9d566f70c2d8ad2462293e64ee170
Anna <anna.andersson.java25@edu.edugrade.se>
2025-10-02 15:33:07 +0200

    Added attributes to Character and Status to Hero

--- src/main/java/org/example/Characters/Hero.java
+++ src/main/java/org/example/Characters/Hero.java
@@ -1,4 +1,18 @@
 package org.example.Characters;
 
-public class Hero {
+public class Hero extends Character{
+    int level = 1;
+
+    public Hero(String name) {
+        super(name);
+    }
+
+    @Override
+    void takeDamage() {
+
+    }
+
+    public void status(){
+        System.out.println(name + " has " + currentHP + " HP left and is level " + level + "!");
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,14 @@
 package org.example;
 
+import org.example.Characters.Hero;
 import org.example.Helpers.Helper;
 import org.example.Helpers.Helpers;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Sunfield Village");
-        Helper helper = new Helper();
-        System.out.println(helper.randomInt());
-        System.out.println("Before sleep");
-        helper.sleepForMilliSeconds(1000);
-        System.out.println("After sleep");
+
+        Hero hero = new Hero("Willhelm");
+        hero.status();
     }
 }
